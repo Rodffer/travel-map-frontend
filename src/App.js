977fe307946d9d8cb42b79bf188b706cf6a606e5
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-12 14:40:46 -0300

    Feat: adicionada api de mapas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import React, { useState } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.6566,
+    longitude: -122.4376,
+    zoom: 4
+  });
   return (
-    <div>Travel Map</div>
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxAccessToken={process.env.MAP_BOX}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+      >
+        <Marker
+          latitude={37}
+          longitude={-122}
+          offsetLeft= {-20}
+          offsetTop= {-10}
+        >
+          <div>Você está aqui</div>
+        </Marker>
+      </ReactMapGL>
+    </div>
   );
 }
 
